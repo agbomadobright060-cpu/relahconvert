5ea2211c38af7e39961e63de0da40e738947b176
agbomadobright060-cpu <agbomadobright060@gmail.com>
2026-02-27 21:54:55 -0700

    fix router path

--- image-converter-app/src/app/router.js
+++ image-converter-app/src/app/router.js
@@ -1,10 +1,6 @@
 import { tools } from '../tools/configs.js'
 
-export function getToolFromPath() {
-  const path = window.location.pathname.replace('/', '').replace('.html', '')
-  return tools[path] || null
-}
-
 export function getCurrentTool() {
-  return getToolFromPath()
+  const path = window.location.pathname.replace(/^\//, '').replace(/\/$/, '')
+  return tools[path] || null
 }
